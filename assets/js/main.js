4b69e0721c51d65c610de20c44f2dc77bf403762
Yunus <sanuyoonus@gmail.com>
2023-07-06 14:36:57 +0530

    added bugs in firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -461,6 +461,8 @@ function closeForm() {
       };
 
 
+
+
             // function SendForm(){
             //     emailjs.send("service_y2dzz7y","template_tmxqj56",{  
             //         name: document.getElementById("name").value ,
@@ -468,3 +470,181 @@ function closeForm() {
             //         email: document.getElementById("email").value
             //         });
             // }
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBuZusdd5iFZK7w0B5-EMKvxAiFiboGmcE",
+//   authDomain: "elizabeth-testimonials.firebaseapp.com",
+//   projectId: "elizabeth-testimonials",
+//   storageBucket: "elizabeth-testimonials.appspot.com",
+//   messagingSenderId: "1053614450199",
+//   appId: "1:1053614450199:web:4f966c8e07879537c1de52",
+//   measurementId: "G-YDF7W4V55X"
+// };
+// Initialize Firebase
+
+//   firebase.initializeApp(firebaseConfig);
+  
+  // Get a reference to the testimonials collection in Firestore
+//   const testimonialsRef = firebase.firestore().collection('testimonials');
+  
+  // Function to render testimonials
+//   function renderTestimonials(testimonials) {
+//     const testimonialsCarousel = document.getElementById('testimonialsCarousel');
+  
+//     testimonials.forEach((testimonial) => {
+//       const item = document.createElement('div');
+//       item.classList.add('item');
+  
+//       const provider = document.createElement('div');
+//       provider.classList.add('provider');
+  
+//       const thumb = document.createElement('div');
+//       thumb.classList.add('thumb');
+//       const thumbImg = document.createElement('img');
+//       thumbImg.src = testimonial.thumb;
+//       thumbImg.alt = 'Thumb';
+//       thumb.appendChild(thumbImg);
+  
+//       const bio = document.createElement('div');
+//       bio.classList.add('bio');
+//       const h5 = document.createElement('h5');
+//       h5.textContent = testimonial.name;
+//       const span = document.createElement('span');
+//       span.innerHTML = `Placed in <strong>${testimonial.company}</strong>`;
+//       bio.appendChild(h5);
+//       bio.appendChild(span);
+  
+//       provider.appendChild(thumb);
+//       provider.appendChild(bio);
+  
+//       const info = document.createElement('div');
+//       info.classList.add('info');
+//       const p = document.createElement('p');
+//       p.style.textAlign = 'justify';
+//       p.textContent = testimonial.content;
+//       info.appendChild(p);
+  
+//       item.appendChild(provider);
+//       item.appendChild(info);
+  
+//       testimonialsCarousel.appendChild(item);
+//     });
+//   }
+  
+  // Function to fetch testimonials from Firestore
+//   function fetchTestimonials() {
+//     testimonialsRef.get().then((querySnapshot) => {
+//       const testimonials = [];
+//       querySnapshot.forEach((doc) => {
+//         testimonials.push(doc.data());
+//       });
+//       renderTestimonials(testimonials);
+//     }).catch((error) => {
+//       console.log('Error getting testimonials: ', error);
+//     });
+//   }
+  
+  // Fetch testimonials when the DOM content is loaded
+//   document.addEventListener('DOMContentLoaded', fetchTestimonials);
+  // Your Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBuZusdd5iFZK7w0B5-EMKvxAiFiboGmcE",
+  authDomain: "elizabeth-testimonials.firebaseapp.com",
+  databaseURL: "https://elizabeth-testimonials-default-rtdb.firebaseio.com",
+  projectId: "elizabeth-testimonials",
+  storageBucket: "elizabeth-testimonials.appspot.com",
+  messagingSenderId: "1053614450199",
+  appId: "1:1053614450199:web:4f966c8e07879537c1de52",
+  measurementId: "G-YDF7W4V55X"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Access Firestore
+var db = firebase.firestore();
+var testimonialForm = document.getElementById('testimonialForm');
+var testimonialsSection = document.getElementById('testimonials');
+
+// Function to add a testimonial to Firestore
+function addTestimonialToFirestore(testimonial) {
+  return db.collection('testimonials').add(testimonial);
+}
+
+// Function to render testimonials on the frontend
+function renderTestimonials(testimonials) {
+  testimonialsSection.innerHTML = '';
+
+  testimonials.forEach((testimonial) => {
+    var figure = document.createElement('figure');
+    figure.className = 'snip1192';
+
+    var blockquote = document.createElement('blockquote');
+    blockquote.style.textAlign = 'justify';
+    blockquote.textContent = testimonial.content;
+
+    var authorDiv = document.createElement('div');
+    authorDiv.className = 'author';
+    authorDiv.style.paddingTop = '43px';
+
+    var authorImg = document.createElement('img');
+    authorImg.src = 'assets/img/nurse.png';
+    authorImg.alt = 'sq-sample1';
+
+    var authorName = document.createElement('h6');
+    authorName.innerHTML = '<strong>' + testimonial.name + '</strong>';
+
+    var placementSpan = document.createElement('span');
+    placementSpan.className = 'span-test';
+    placementSpan.innerHTML = 'Placed in <strong>' + testimonial.placement + '</strong>';
+
+    authorDiv.appendChild(authorImg);
+    authorDiv.appendChild(authorName);
+    authorDiv.appendChild(placementSpan);
+
+    figure.appendChild(blockquote);
+    figure.appendChild(authorDiv);
+
+    testimonialsSection.appendChild(figure);
+  });
+}
+
+// Event listener for form submission
+testimonialForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Get form values
+  var name = document.getElementById('name').value;
+  var placement = document.getElementById('placement').value;
+  var content = document.getElementById('content').value;
+
+  // Create testimonial object
+  var testimonial = {
+    name: name,
+    placement: placement,
+    content: content
+  };
+
+  // Add testimonial to Firestore
+  addTestimonialToFirestore(testimonial)
+    .then(function() {
+      // Clear form fields after successful submission
+      testimonialForm.reset();
+    })
+    .catch(function (error) {
+      console.error('Error adding testimonial:', error);
+    });
+});
+
+// Real-time listener for testimonial updates
+db.collection('testimonials')
+  .onSnapshot(function (querySnapshot) {
+    var testimonials = [];
+
+    querySnapshot.forEach(function (doc) {
+      var testimonial = doc.data();
+      testimonials.push(testimonial);
+    });
+
+    renderTestimonials(testimonials);
+  });
